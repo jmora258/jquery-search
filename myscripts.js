6cf6d03f067b3811f9deb58b1e6e8f1840207aca
Justino Mora <jmora258@gmail.com>
2016-02-15 16:54:36 -0800

    Complete and test live search. Success.

--- myscripts.js
+++ myscripts.js
@@ -1,18 +1,32 @@
 // My scripts: 
-alert("hello!"); 
-$('#update').html("jquery is working"); 
+$(document).ready( function() {
 
+	$('#search').keyup( function() {
 
-$(document).ready(function() {
-			
-    $('#mybutton').click(function(event){
-        $.getJSON('data.json', function(data) {
-            alert("success!! Yay!"); 
-            console.log(data);
-        });
-    });
+		var searchQuery = $(this).val();  // retrieve each key value 
+		var myRegExp = new RegExp(searchQuery, "i"); // case-insensitive 
+		
+		$.getJSON('data.json', function(data) {
 
-    console.log("hey there! From console"); 
+			var output = '<ul class="results">'; 
 
+			$.each(data, function(key, value) {
 				
-});
+				if( (this.name.search(myRegExp)) != -1 || (this.bio.search(myRegExp)) != -1) {
+					output += '<li>';
+					output += '<h2>' + this.name + '</h2>'; 
+					output += '<img src="' + 'images/candidates2016/' + this.firstName + '_'+ this.lastName 
+						   + '_Small.jpg" atl="' + this.name + '" />'; 
+				 	output += '<p>' + this.bio + '</p>';
+					output += '</li>'; 
+				}
+			}); 
+
+			output += '</ul>'; 
+			$('#update').html(output); 
+
+		});	// retrieve JSON
+
+	}); // retrive search query from search bar
+
+}); // wait for document to load 
